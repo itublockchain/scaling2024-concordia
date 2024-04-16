cc29e34a5e026364a6b6740c7e37893a6ab4c5dd
Ataberk <ataberkcekic@hotmail.com.tr>
2024-04-16 18:15:44 +0300

    starter nextjs

---
+++ frontend/utils/binding.ts
@@ -0,0 +1,19 @@
+function get_projects() {}
+
+function show_project_info() {}
+
+function create_project() {}
+
+function create_account() {}
+
+function apply_for_project() {}
+
+function closeProject() {}
+
+function editAccount() {}
+
+function editProject() {}
+
+function reject_application() {}
+
+function accept_application() {}
