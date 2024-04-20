f1edd87fcbf682e7ee64f0fa712145ab73a70446
barisbice1 <113785436+barisbice1@users.noreply.github.com>
2024-04-20 15:06:33 +0300

    projectpage

--- frontend/app/_components/Header.tsx
+++ frontend/app/_components/Header.tsx
@@ -111,12 +111,7 @@ export default function Header() {
               required
             />
             <div className="dropdown inline-block relative">
-              <button
-                onClick={toggleDropdown}
-                className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Who are you looking for?
-              </button>
+  
               {showDropdown && (
                 <div className="dropdown-content absolute z-10">
                   <a
